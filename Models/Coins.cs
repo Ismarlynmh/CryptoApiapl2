ef969e69f5d55b2c2a65a0a468c6f54879cd4d32
Ismarlynmh <ismarlyn123@gmail.com>
2022-07-04 18:27:54 -0300

    Agregar archivos de proyecto.

---
+++ Models/Coins.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CryptoApiapl2.Models
+{
+    public class Coins
+    {
+        [Key]
+        public int MonedaId { get; set; }
+        public String? Descripcion { get; set; }
+        public double? Valor { get; set; }
+        public String? ImageUrl { get; set; }
+
+    }
+}
